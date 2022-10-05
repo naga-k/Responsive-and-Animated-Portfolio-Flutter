084ae3512c46e1eaf5c8a42b3988bc31a656c0b4
Naga Anjaneyulu Karumuri <anjaneyulun02@gmail.com>
2022-10-04 23:49:20 -0400

    Side info created

--- lib/screens/main/main_screen.dart
+++ lib/screens/main/main_screen.dart
@@ -1,10 +1,36 @@
 import 'package:flutter/material.dart';
 
+import 'components/side_menu.dart';
+
 class MainScreen extends StatelessWidget {
   const MainScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Row(
+        children: [
+          Expanded(flex: 2, child: SideDrawer()),
+          Expanded(
+              flex: 7,
+              child: Container(
+                color: Colors.blue,
+              )),
+        ],
+      ),
+    );
+  }
+}
+
+class SideDrawer extends StatelessWidget {
+  const SideDrawer({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: Column(children: [myInfo()]),
+    );
   }
 }
